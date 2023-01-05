e112f5dd1038d6b400dd08a8aac132c991160310
Jim Clark <jim@atomist.com>
2023-01-05 11:41:34 -0800

    Fix sample again

--- dev/user.clj
+++ dev/user.clj
@@ -22,22 +22,5 @@
 ;; run sbom generation on local image
 (docker/sbom "vonwig/clojure-base:jdk17" (fn [event] (println event)))
 
-(defn transact-hashes [{:keys [image digest diff-id->digest transaction-url token]}]
-  (println "")
-  (let [all-hashes (atom [])]
-    (docker/hashes image (fn [event]
-                           (if (= "done" (:status event))
-                             (let [tx-data (->> @all-hashes
-                                                (mapcat (fn [{:keys [path hash diff-id]}]
-                                                          (let [blob-digest (diff-id->digest diff-id)]
-                                                            [{:schema/entity blob-digest
-                                                              :schema/entity-type :docker.image/blob
-                                                              :docker.image.blob/digest digest}
-                                                             {:docker.image.blob.file/sha256 hash
-                                                              :docker.image.blob.file/blob digest}]))))]
-                               (println "transact " tx-data)
-                               (println "transact " [{:docker.image/digest digest
-                                                      :schema/entity-type :docker/image
-                                                      :malware.status/indexed :malware.status.indexed/complete}]))
-                             (swap! all-hashes conj event))))))
+(docker/hashes "vonwig/malware1:latest" (fn [event] (println event)))
 
